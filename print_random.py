c0fa5aae8a0011805cd36f8dede463af1ec6b71a
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-02-25 23:07:04 +0100

    initial commit

---
+++ print_random.py
@@ -0,0 +1,14 @@
+
+
+#define a function
+def random_function(param1, param2):
+    print(f"Parameter 1: {param1}")
+    print(f"Parameter 2: {param2}")
+
+def main():
+    some_variable = "Hello, World!"
+    some_random_number = 42
+    random_function(some_variable, some_random_number)
+
+
+if __name__ == "__main__":    main()
