79924a2033a673f427b31e778dd9a043d09325a0
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-03-05 23:46:23 +0100

    commit changes on branch

--- src/print_random.py
+++ src/print_random.py
@@ -1,4 +1,7 @@
 #define a function
+def hello_world():
+    print("Hello, World!")
+
 def random_function(param1, param2):
     print(f"Parameter 1: {param1}")
     print(f"Parameter 2: {param2}")
@@ -8,6 +11,8 @@ def main():
     some_variable = "Hello, World!"
     some_random_number = 42
     random_function(some_variable, some_random_number)
+    hello_world()
+    print("This is a random print statement.")
 
 
 if __name__ == "__main__":    
