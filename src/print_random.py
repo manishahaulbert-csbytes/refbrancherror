c9da5b3372aa4be6dff61ff131e4074e98f48eca
Manisha Haulbert <manisha.haulbert@sonarsource.com>
2026-03-06 17:30:06 +0100

    changes on featurebranch for no issues

--- src/print_random.py
+++ src/print_random.py
@@ -2,16 +2,15 @@
 def hello_world():
     print("Hello, World!")
 
-def add_numbers(a, b):
-    break # This is a syntax error, but we are ignoring it for this example.
-    return a + b
-    
-
 def random_function(param1, param2):
     print(f"Random function called with parameters: {param1} and {param2}")
     if param2 > 40:
-        break # This is a syntax error, but we are ignoring it for this example.
+        break
     return param1.upper()
+
+def another_random_function():
+    print("This is another random function.")
+    return "Random Result"
 
 
 def main():
@@ -20,9 +19,7 @@ def main():
     random_function(some_variable, some_random_number)
     hello_world()
     print("This is a random print statement.")
-    add_numbers(5, 10)
 
 
 if __name__ == "__main__":    
     main()
-    
